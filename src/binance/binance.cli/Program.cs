18301c68bb6569ec0150ffcca1436f84b59b7cae
Damian Wolgast <damian.wolgast@si-co.net>
2020-03-03 19:22:51 +0100

    Initial commit using BinanceDotNet

---
+++ src/binance/binance.cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace binance.cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
