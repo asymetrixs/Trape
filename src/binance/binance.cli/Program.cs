5caacd673a6664b55a4851c97a35a14aa63060c1
Damian Wolgast <damian.wolgast@si-co.net>
2020-03-03 19:35:57 +0100

    Added Microsoft configuration

--- src/binance/binance.cli/Program.cs
+++ src/binance/binance.cli/Program.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 
 namespace binance.cli
 {
     class Program
     {
+        public static IConfigurationRoot Configuration;
+
         static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("settings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
             Console.WriteLine("Hello World!");
         }
     }
