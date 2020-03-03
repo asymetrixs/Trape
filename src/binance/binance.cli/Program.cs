b48c94843a2df75d758a15b04caa3bdcc8958b95
Damian Wolgast <damian.wolgast@si-co.net>
2020-03-03 19:50:13 +0100

    Fooling around

--- src/binance/binance.cli/Program.cs
+++ src/binance/binance.cli/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using BinanceExchange.API.Client;
+using log4net;
+using Microsoft.Extensions.Configuration;
 using System;
+using System.Threading.Tasks;
 
 namespace binance.cli
 {
@@ -7,13 +10,42 @@ namespace binance.cli
     {
         public static IConfigurationRoot Configuration;
 
-        static void Main(string[] args)
+        async static Task Main(string[] args)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
                 .AddJsonFile("settings.json", optional: false, reloadOnChange: true);
 
             Configuration = builder.Build();
+
+            var exampleProgramLogger = LogManager.GetLogger(typeof(Program));
+            exampleProgramLogger.Debug("Logging Test");
+
+            //Initialise the general client client with config
+            var client = new BinanceClient(new ClientConfiguration()
+            {
+                ApiKey = Configuration.GetSection("binance:apikey").Value,
+                SecretKey = Configuration.GetSection("binance:secretkey").Value,
+                Logger = exampleProgramLogger
+            });
+
+            // Test the Client
+            //var response = await client.TestConnectivity();
+            
+            //var exchangeInfo = await client.GetExchangeInfo();
+
+            //var systemStatus = await client.GetSystemStatus();
+
+            //var symbolsPriceTicker = await client.GetSymbolsPriceTicker();
+
+            var btcPrice = await client.GetPrice("BTCUSDT");
+            var candleStick = await client.GetKlinesCandlesticks(new BinanceExchange.API.Models.Request.GetKlinesCandlesticksRequest()
+            {
+                StartTime = DateTime.UtcNow.AddDays(-1),
+                Interval = BinanceExchange.API.Enums.KlineInterval.OneMinute,
+                Symbol = "BTCUSDT",
+                EndTime = DateTime.UtcNow
+            });
 
             Console.WriteLine("Hello World!");
         }
