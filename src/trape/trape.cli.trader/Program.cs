80bf8559fcbb154df2bbeacf8adea23fd7a071d2
Damian Wolgast <damian.wolgast@si-co.net>
2020-03-09 17:01:18 +0100

    Added trading cli

---
+++ src/trape/trape.cli.trader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace trape.cli.trader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
