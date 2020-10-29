a75f4dfece571d6fd70f196dee373c9ee1e4489a
Damian Wolgast <damian@si-co.net>
2020-10-29 12:53:03 +0100

    Initial web Api and Vue projects

---
+++ src/trape/Trape.Api.ControlCenter/Config.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Trape.Api.ControlCenter
+{
+    /// <summary>
+    /// This class holds settings.json
+    /// </summary>
+    public static class Config
+    {
+        #region Properties
+
+        /// <summary>
+        /// Current settings.json
+        /// </summary>
+        public static IConfigurationRoot Current { get; private set; }
+
+        #endregion
+
+        #region
+
+        /// <summary>
+        /// Initializes the configuration from settings.json
+        /// </summary>
+        public static void SetUp()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Current = builder.Build();
+        }
+
+        public static string GetValue(string section)
+        {
+            return Current.GetSection(section).Value;
+        }
+
+        public static string GetConnectionString(string connectionName)
+        {
+            return Current.GetConnectionString(connectionName);
+        }
+
+        #endregion
+    }
+}
---
+++ src/trape/Trape.Api.ControlCenter/Controllers/StatusController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Trape.Api.ControlCenter.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatusController : ControllerBase
+    {
+        private readonly ILogger<StatusController> _logger;
+
+        public StatusController(ILogger<StatusController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public StatusCodeResult Get()
+        {
+            return Ok();
+        }
+    }
+}
